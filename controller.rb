723f545cb353438d0a6f37eaac44ca19ab262565
dan <dancolyer@icloud.com>
2017-10-16 16:32:25 +0100

    Created Game class

---
+++ controller.rb
@@ -0,0 +1,24 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require_relative("./models/calculator.rb")
+
+get "/game/:value1/:value2" do
+  game = Game.new(params[:value1], params[:value2])
+end
+
+
+
+# get "/add/:num1/:num2" do
+#   calculator = Calculator.new(params[:num1].to_i, params[:num2].to_i)
+#   #return calculator.add().to_s
+#   @calculation = calculator.add()
+#   erb(:result)
+# end
+
+# Create a simple sinatra app to allow the user to play rock, paper, scissors in their browser.
+#
+#     You should be able to go to /rock/scissors and return the string "rock wins" to the page, for example.
+#
+#     You will need to write a bit of logic that compares the two hands and decides which one has won, and then use that in the route definition.
+#
+#     Next, create a models directory for your game logic class e.g. 'Game' and put all of the logic in there - there shouldn't be any logic left in the controller!
---
+++ models/game.rb
@@ -0,0 +1,27 @@
+class Game
+
+  def initialize(value1, value2)
+    @value1 = value1
+    @value2 = value2
+  end
+
+  def outcome()
+    if (@value1 = @value2)
+      "Draw"
+    elsif ((@value1 = "rock") && (@value2 = "scissors"))
+      "Rock Wins!"
+    elsif ((@value1 = "scissors") && (@value2 = "rock"))
+      "Rock Wins!"
+    elsif ((@value1 = "rock") && (@value2 = "paper"))
+      "Paper Wins!"
+    elsif ((@value1 = "paper") && (@value2 = "rock"))
+      "Paper Wins!"
+    elsif ((@value1 = "paper") && (@value2 = "scissors"))
+      "Scissors Wins!"
+    elsif ((@value1 = "scissors") && (@value2 = "paper"))
+      "Scissors Wins!"
+    end
+  end
+
+
+end
